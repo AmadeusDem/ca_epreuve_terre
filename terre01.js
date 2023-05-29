0f988b3a29e314b3e310f5dfbf6b8ffb1bc138f6
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-05-29 12:52:13 +0200

    🏔️  Changed the code to train string manipulation

--- terre01.js
+++ terre01.js
@@ -1,2 +1,15 @@
-let path = require('path');
-console.log(path.basename(__filename));
+/**
+Créez un programme qui affiche son nom de fichier.
+
+
+Exemples d’utilisation :
+$> node exo.js
+exo.js
+
+$> node crevette.js
+crevette.js
+
+ */
+
+let path = process.argv[1];
+console.log(path.split('/')[6]);
