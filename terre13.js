e083ed4a7a6220e5a071a71995949275e642e8c7
AmadeusDem <39994816+AmadeusDem@users.noreply.github.com>
2023-06-05 12:55:29 +0200

    🏔️ Refactoring of this one to respect DRY coding

--- terre13.js
+++ terre13.js
@@ -26,32 +26,24 @@ const findMiddleValue = (number1, number2, number3) => {
   if (number1 === number2 && number1 === number3) {
     return 0;
   }
-  if (number1 > number2) {
-    if (number2 > number3) {
-      return number2;
-    } else if(number3 > number1) {
-      return number3;
-    } else {
-      return number1;
-    }
+  if((number1 < number2 && number2 < number3) || (number1 > number2 && number2 > number3)) {
+    return number2;
+  } else if ((number1 < number2 && number3 < number1) || (number1 < number3 && number2 < number1)) {
+    return number1;
   } else {
-    if (number1 > number3) {
-      return number1;
-    } else if (number2 > number3) {
-      return number3;
-    } else {
-      return number2;
-    }
+    return number3;
   }
 }
-let number1 = parseInt(args[2]),
-    number2 = parseInt(args[3]),
-    number3 = parseInt(args[4]);
+
+const number1 = parseInt(args[2]);
+const number2 = parseInt(args[3]);
+const number3 = parseInt(args[4]);
+
 if (isNaN(number1) || isNaN(number2) || isNaN(number3)) {
   printError();
   return;
 }
-let test = findMiddleValue(number1, number2, number3);
+const test = findMiddleValue(number1, number2, number3);
 if(test !== 0){
   console.log(test);
 } else {
